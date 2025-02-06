52506bea6341c85ef40ec2598798a5cf8ad093f7
p.luciani <paulluciani76@gmail.com>
2025-02-06 12:51:03 +0100

    Split CSV and html

--- handleCSV.R
+++ handleCSV.R
@@ -23,21 +23,3 @@ m$insert(flights)
 
 # Vérification
 print(m$count())
-
-
-#Chargement des planes avec le fichier html
-library(rvest)
-
-# Charger le fichier HTML
-page <- read_html("F:/Cours/R/project_r/dataset_flights/planes.html")
-
-# Extraire une table HTML
-planes <- page %>% html_table(fill = TRUE)
-
-# Afficher les données
-print(planes)
-
-m <- mongo(collection = "planes", db = "flights", url = "mongodb://localhost:27017")
-
-# Insérer les données dans MongoDB
-m$insert(planes[[1]])
